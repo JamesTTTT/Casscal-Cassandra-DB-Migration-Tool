eaf0bea9b252cc86833319ce35aca13f9ffa3751
JamesTTTT <taylormarkaryd@gmail.com>
2024-02-17 17:47:55 +0100

    Included CommandLine parser in MigrationTool

--- src/main/scala/MigrationTool.scala
+++ src/main/scala/MigrationTool.scala
@@ -1,16 +1,30 @@
+import casscal.connector.CassandraConnector
+import casscal.parser.CommandLineParser
 import com.datastax.oss.driver.api.core.CqlSession
 import java.nio.file.{Files, Paths}
-import scala.jdk.CollectionConverters._
+import scala.jdk.CollectionConverters.*
 object MigrationTool {
   def main(args: Array[String]): Unit = {
-    val migrationFolderPath = if (args.length > 0) args(0) else "migrations"
-    val session = CassandraConnector.createSession()
-    try {
-      val migrationFiles = getMigrationFiles("migrations")
-      migrationFiles.foreach(applyMigration(_, session))
-    } finally {
-      session.close()
+    CommandLineParser.cmdParse(args) match {
+      case Some(config) =>
+        val session = CassandraConnector.createSession(
+          contactPoints = config.contactPoints.toList,
+          port = config.port,
+          keyspace = config.keyspace,
+          username = config.username,
+          password = config.password
+        )
+        val migrationFolderPath = config.migrationFilePath
+
+        try {
+          val migrationFiles = getMigrationFiles(migrationFolderPath)
+          migrationFiles.foreach(applyMigration(_, session))
+        } finally {
+          session.close()
+        }
+      case _=>
     }
+
   }
 
   private def getMigrationFiles(folderPath: String): List[String] = {
