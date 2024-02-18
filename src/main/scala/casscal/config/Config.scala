5db5a4b09f9b9514dc44f3439a41ad968a81482d
JamesTTTT <taylormarkaryd@gmail.com>
2024-02-18 17:16:41 +0100

    Added support for datacenter

--- src/main/scala/casscal/config/Config.scala
+++ src/main/scala/casscal/config/Config.scala
@@ -5,4 +5,5 @@ case class Config(username: Option[String] = None,
                   keyspace: Option[String] = None,
                   port: Int = 9042,
                   contactPoints: Seq[String] = Seq("127.0.0.1"),
+                  localDatacenter: String = "datacenter1",
                   migrationFilePath: String = "./migrations")
