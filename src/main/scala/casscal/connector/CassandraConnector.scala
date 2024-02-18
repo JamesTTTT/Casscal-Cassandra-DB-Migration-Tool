a47798ecaed88edddb55dcb87e228eb5bbc17736
JamesTTTT <taylormarkaryd@gmail.com>
2024-02-18 17:15:18 +0100

    Added support for local datacenter & refactored addContactPoints

--- src/main/scala/casscal/connector/CassandraConnector.scala
+++ src/main/scala/casscal/connector/CassandraConnector.scala
@@ -3,18 +3,20 @@ package casscal.connector
 import com.datastax.oss.driver.api.core.CqlSession
 import com.datastax.oss.driver.internal.core.ContactPoints
 import sun.security.util.Password
-
+import scala.jdk.CollectionConverters._
 import java.net.InetSocketAddress
 object CassandraConnector {
   def createSession(contactPoints: List[String],
                     port: Int,
                     keyspace: Option[String] = None,
                     username: Option[String] = None,
-                    password: Option[String] = None): CqlSession = {
+                    password: Option[String] = None,
+                    localDatacenter: String
+                   ): CqlSession = {
 
     val builder = CqlSession.builder()
-
-    contactPoints.foreach(cp => builder.addContactPoint(new InetSocketAddress(cp, port)))
+      .addContactPoints(contactPoints.map(InetSocketAddress.createUnresolved(_,port)).asJava)
+      .withLocalDatacenter(localDatacenter)
 
     keyspace.foreach(builder.withKeyspace)
 
